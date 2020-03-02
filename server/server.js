465ce5df122b431d4fd8877fda01c4f9ed244a66
Paul Wei <pwei1@ufl.edu>
2020-03-01 21:52:18 -0500

    Setting up web app

--- server/server.js
+++ server/server.js
@@ -1,7 +1,36 @@
-const express = require('./config/express.js')
- 
-// Use env port or default
-const port = process.env.PORT || 5000;
+import path from 'path';
+import express from 'express';      //Require satement  --const express = require('./config/express.js')
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import config from './config/config.js';
 
-const app = express.init()
-app.listen(port, () => console.log(`Server now running on port ${port}!`));
+
+// Use env port or default  --const port = process.env.PORT || 5000;
+
+//connect to database if needed
+/*mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
+    console.log('Successfully connected to mongoose database.')
+});*/
+
+//Old initialization of app middleware  --const app = express.init()
+const app = express();
+
+//enable request logging for development debugging
+app.use(morgan('dev'));
+
+//body parsing middleware
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//serve static files as needed
+app.use('/', express.static('./../../client'));
+
+/* Request handler for all other routes - default for now 
+   Sends a response (res) to go to the homepage for all routes not specified */
+app.all('/*', (req,res) => {
+    res.sendFile(path.resolve(config.folderPath,'./../../client/public/index.html'));
+});
+
+app.listen(config.port, () => console.log(`Server now running on port ${config.port}!`));
