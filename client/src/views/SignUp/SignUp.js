a31b9d1383f427c1a7bfad561ef4fdda6a9f4502
Amaya Jimenez <amayanicolejimenez@gmail.com>
2020-03-08 17:26:28 -0400

    All inputs for user form + button are done. Need to style. Good time to begin backend implementation

--- client/src/views/SignUp/SignUp.js
+++ client/src/views/SignUp/SignUp.js
@@ -23,6 +23,34 @@ const SignUp = () => {
                     <input type="text" className="form-control" placeholder="Location of Birth"/>
                 </div>
             </div>
+            <div className="form-row">
+                <div className="col">
+                    <label htmlFor="timeOfBirth-label">Time of Birth</label>
+                    <input className="form-control" type="time" id="timeOfBirth-input"/>
+                </div>
+            </div>
+            <div className="form-row">
+                <div className="col">
+                    <input type="text" className="form-control" type = "email" placeholder="Email Address"/>
+                </div>
+                <div className="col">
+                    <input type="text" className="form-control" type = "tel" placeholder="Phone Number"/>
+                </div>
+            </div>
+            <div className="form-row">
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Username"/>
+                </div>
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Password"/>
+                </div>
+            </div>
+            <div className="form-row">
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Confirm Password"/>
+                </div>
+            </div>
+            <button type="button" className="btn btn-outline-primary">Primary</button>
         </form>
 
     );
