2dff9a41cf7dee8a813293e58014d3c2ea91930a
Amaya Jimenez <amayanicolejimenez@gmail.com>
2020-03-10 01:30:08 -0400

    I think the errors went away?

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
-// import path from 'path';
-// import express from 'express';      //Require satement  --const express = require('./config/express.js')
-// import mongoose from 'mongoose';
-// import morgan from 'morgan';
-// import bodyParser from 'body-parser';
-// import config from './config/config.js';
-// import cors from 'cors';
-// import newUserController from "./controllers/newUserController.js";
+import path from 'path';
+import express from 'express';      //Require satement  --const express = require('./config/express.js')
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import config from './config/config.js';
+import cors from 'cors';
+import newUserController from "./controllers/newUserController.js";
 
 
 // Use env port or default --const port = process.env.PORT || 5000;
-//
-// //connect to database if needed
-// mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
-//     console.log(`Successfully connected to mongoose database.`)
-// });
-// //Old initialization of app middleware  --const app = express.init()
-// const app = express();
-//
+
+//connect to database if needed
+mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
+    console.log(`Successfully connected to mongoose database.`)
+});
+//Old initialization of app middleware  --const app = express.init()
+const app = express();
+
 // //enable request logging for development debugging
 // app.use(morgan('dev'));
 //
@@ -39,13 +39,5 @@
 // app.all('/*', (req,res) => {
 //     res.sendFile(path.resolve('./client/public/index.html'));
 // });
-//
-// app.listen(config.port, () => console.log(`Server now running on port ${config.port}!`));
 
-const express = require('./config/express.js')
-
-// Use env port or default
-const port = process.env.PORT || 5000;
-
-const app = express.init()
-app.listen(port, () => console.log(`Server now running on port ${port}!`));
+app.listen(config.port, () => console.log(`Server now running on port ${config.port}!`));
