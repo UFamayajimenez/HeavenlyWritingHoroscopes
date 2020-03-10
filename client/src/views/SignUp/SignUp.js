93cd21decf41e1723bcb21f07649901803c2d918
Logan Welsh <lrdwelsh98@gmail.com>
2020-03-10 11:20:26 -0400

    broke

--- client/src/views/SignUp/SignUp.js
+++ client/src/views/SignUp/SignUp.js
@@ -16,7 +16,7 @@ const SignUp = () => {
     };
 
     const handleSubmit = () => {
-        const url = "http://localhost:80/SignUp";
+        const url = "http://localhost:5000/SignUp";
         axios.post(url, data)
             .then(res => {
                 console.log('post request sent');
--- server/server.js
+++ server/server.js
@@ -49,5 +49,5 @@ const express = require('./config/express.js');
 
 const app = express.init();
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server now running on port 5000!`));
+app.listen((process.env.PORT || 5000), () => console.log(`Server now running on port 5000!`));
 
