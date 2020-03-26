0047df637af981cbb3b4e4ae8db65f6a3aab0f96
Paul Wei <pwei1@ufl.edu>
2020-03-26 17:38:58 -0400

    changes for updating to ES6

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './views/SignUp/SignUp.css'
-import App from './App';
+import App from './App.js';
 
 ReactDOM.render(
     <Router>
--- server/server.js
+++ server/server.js
@@ -1,53 +1,57 @@
 // import path from 'path';
-// // // import express from 'express';      //Require satement  --const express = require('./config/express.js')
-// // // import mongoose from 'mongoose';
-// // // import morgan from 'morgan';
-// // // import bodyParser from 'body-parser';
-// // // import config from './config/config.js';
-// // // import cors from 'cors';
-// // // import newUserController from "./controllers/newUserController.js";
-// // //
-// // //
-// // // // Use env port or default --const port = process.env.PORT || 5000;
-// // //
-// // // //connect to database if needed
-// // // mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
-// // //     console.log(`Successfully connected to mongoose database.`)
-// // // });
-// // // //Old initialization of app middleware  --const app = express.init()
-// // // const app = express();
-// // //
-// // // //enable request logging for development debugging
-// // // app.use(morgan('dev'));
-// // //
-// // // //body parsing middleware
-// // // app.use(bodyParser.urlencoded({
-// // //     extended: true
-// // // }));
-// // //
-// // // app.use(bodyParser.json());
-// // //
-// // // app.use(cors());
-// // //
-// // // //serve static files as needed
-// // // app.use('/', express.static('./../../client'));
-// // //
-// // // app.post('/SignUp', newUserController);
-// // //
-// // // /* Request handler for all other routes - default for now
-// // //    Sends a response (res) to go to the homepage for all routes not specified */
-// // // app.all('/*', (req,res) => {
-// // //     res.sendFile(path.resolve('./client/public/index.html'));
-// // // });
-// // //
-// // // app.listen(config.port, () => console.log(`Server now running on port ${config.port}!`));
+import express from 'express';      //Require satement  --const express = require('./config/express.js')
+// import mongoose from 'mongoose';
+// import morgan from 'morgan';
+// import bodyParser from 'body-parser';
+import config from './config/config.js';
+// import cors from 'cors';
+// import newUserController from "./controllers/newUserController.js";
+//
+//
+// // Use env port or default --const port = process.env.PORT || 5000;
+//
+// //connect to database if needed
+// mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
+//     console.log(`Successfully connected to mongoose database.`)
+// });
+// //Old initialization of app middleware  --const app = express.init()
+const app = express();
+//
+// //enable request logging for development debugging
+// app.use(morgan('dev'));
+//
+// //body parsing middleware
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+//
+// app.use(bodyParser.json());
+//
+// app.use(cors());
+//
+// //serve static files as needed
+// app.use('/', express.static('./../../client'));
+//
+// app.post('/SignUp', newUserController);
+//
+// /* Request handler for all other routes - default for now
+//    Sends a response (res) to go to the homepage for all routes not specified */
+// app.all('/*', (req,res) => {
+//     res.sendFile(path.resolve('./client/public/index.html'));
+// });
+//
+// app.listen(config.port, () => console.log(`Server now running on port ${config.port}!`));
 
 
 //const config = require('./config/config.js');
 
-const express = require('./config/express.js');
+// const express = require('./config/express.js');
 
-const app = express.init();
+// const app = express.init();
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Welcome to Node.js & Express" });
+});
+
 
 app.listen(process.env.PORT || 5000, () => console.log(`Server now running on port 5000!`));
 
