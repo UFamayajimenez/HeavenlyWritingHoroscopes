f6bae16085e7c86bd981e7bc109c9a03aef6c836
James Boultinghouse <jboultinghouse@ufl.edu>
2020-04-01 00:05:37 -0400

    Patched ClientNav

--- client/src/views/Admin/AdminNavBar.js
+++ client/src/views/Admin/AdminNavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 
 const AdminNavBar = () => {
 
@@ -7,22 +8,51 @@ const AdminNavBar = () => {
     //     do something here about signing out
     // }
 
+    const logOut = () => {
+        sessionStorage.setItem("loggedStatus", 0);
+        console.log("Logged Status: " + sessionStorage.getItem("loggedStatus"));
+    }
+
+    if (sessionStorage.getItem("loggedStatus") == 0){
+        return <Redirect to='/Home' />
+    }
+
     return ( 
-        <Navbar bg="dark" expand="lg" variant="dark" >
-            <Navbar.Brand href="/admin/users">Heavenly Writing</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/admin/users">Users</Nav.Link>
-                    <Nav.Link href="/admin/database">Database</Nav.Link>
-                    <Nav.Link href="/admin/overview">Overview</Nav.Link>
-                    <Nav.Link href="/admin/statistics">Statistics</Nav.Link>
-                    <Nav.Link href="/admin/settings">Settings</Nav.Link>
-                    <Nav.Link href="/admin/create">Create Email</Nav.Link>
-                    <Nav.Link href="/admin/signout" style={{color: "red"}}>Log Out</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+            <a className="navbar-brand" href="/Home">Heavenly Writing</a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/admin/users">Users <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/admin/database">Database</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/admin/overview">Overview</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/admin/statistics">Statistics</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/admin/settings">Settings</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/admin/create">Create Email</a>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link" onClick={logOut}>Log Out</button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
--- client/src/views/Client/ClientNavBar.js
+++ client/src/views/Client/ClientNavBar.js
@@ -8,17 +8,29 @@ const ClientNavBar = () => {
     // }
 
     return ( 
-        <Navbar bg="dark" expand="lg" variant="dark" >
-            <Navbar.Brand href="/client/todays-report">Heavenly Writing</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/client/todays-report">Today's Report</Nav.Link>
-                    <Nav.Link href="/client/settings">Settings</Nav.Link>
-                    <Nav.Link href="/client/signout" style={{color: "red"}}>Log Out</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+            <a className="navbar-brand" href="/Home">Heavenly Writing</a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/client/todays-report">Today's Report <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/client/Settings">Settings</a>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link">Log Out</button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
