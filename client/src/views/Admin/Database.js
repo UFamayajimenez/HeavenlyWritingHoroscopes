0ea2e58c58db22895c1afb6d6d672df9e9aecece
James Boultinghouse <jboultinghouse@ufl.edu>
2020-04-10 17:30:54 -0400

    Began Database frontend

--- client/src/views/Admin/Database.js
+++ client/src/views/Admin/Database.js
@@ -1,15 +1,33 @@
 import { Redirect } from 'react-router-dom';
 import React, {useState} from 'react';
+import EmailHistory from './EmailHistory';
+
 
 const Database= (props) => {
+
+
+    const ListEmails = () => {
+
+        var email = "This is test information that is not the final implementation by any means"
+
+        return(
+            
+            <EmailHistory/>
+            
+        )
+    }
 
     if (sessionStorage.getItem("loggedStatus") != 2){
         return <Redirect to='/Home' />
     }
     
     return(
-        <div>
-
+        <div className ="adminBackground">
+            <div>
+                <h1 style={{textAlign: "center", color: "white"}}> Past Email Database</h1>
+            </div>
+            <div>{ListEmails()}</div>
+            
         </div>
     );
 }
---
+++ client/src/views/Admin/EmailHistory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmailHistory = (props) => {
+    return(
+        <div className="datatable-container">
+            
+        </div>
+    )
+}
+
+export default EmailHistory;
