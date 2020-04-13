287f5f3ed3aa2f7498a7792e869a155b053dbe12
Logan Welsh <lrdwelsh98@gmail.com>
2020-04-13 13:35:42 -0400

    basic implementation, need to validate input fields

--- server/routes/api/emails.js
+++ server/routes/api/emails.js
@@ -1,10 +1,17 @@
 const express = require('express'),
-    email = require('../../models/EmailSchema.js');
+    EmailModel = require('../../models/EmailSchema.js');
 
 const router = express.Router();
 
 router.post("/create", (req, res) => {
-    
+    console.log(req.body);
+    EmailModel.create(req.body, (err, newEmail) => {
+        if (err) console.log(err);
+        else {
+            console.log('New email created: \n', newEmail);
+            res.send('Success!');
+        }
+    })
 });
 
 module.exports = router;
