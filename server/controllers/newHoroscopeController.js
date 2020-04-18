753f48e44f7d1b958b262e144039ff704164734f
jtlsc-net <jlumshuechan@ufl.edu>
2020-04-18 16:58:51 -0400

    ascendant calculator works now'

--- server/controllers/newHoroscopeController.js
+++ server/controllers/newHoroscopeController.js
@@ -22,27 +22,57 @@ const getInfo = function(req, res){
             }
             else if(30 <= houses.ascendant < 60){
                 user.overwrite({natalSign: 'Taurus'}).save(() => {
-                    console.log("use is ", user.natalSign);
+                    console.log("user is ", user.natalSign);
                 });
             }
             else if(60 <= houses.ascendant < 90){
                 user.overwrite({natalSign: 'Gemini'}).save(() => {
-                    console.log("use is ", user.natalSign);
+                    console.log("user is ", user.natalSign);
                 });
             }
             else if(90 <= houses.ascendant < 120){
                 user.overwrite({natalSign: 'Cancer'}).save(() => {
-                    console.log("use is ", user.natalSign);
+                    console.log("user is ", user.natalSign);
                 });
             }
             else if(120 <= houses.ascendant < 150){
                 user.overwrite({natalSign: 'Leo'}).save(() => {
-                    console.log("use is ", user.natalSign);
+                    console.log("user is ", user.natalSign);
                 });
             }
             else if(150 <= houses.ascendant < 180){
                 user.overwrite({natalSign: 'Virgo'}).save(() => {
-                    console.log("use is ", user.natalSign);
+                    console.log("user is ", user.natalSign);
+                });
+            }
+            else if(180 <= houses.ascendant < 210){
+                user.overwrite({natalSign: 'Libra'}).save(() => {
+                    console.log("user is ", user.natalSign);
+                });
+            }
+            else if(210 <= houses.ascendant < 240){
+                user.overwrite({natalSign: 'Scorpio'}).save(() => {
+                    console.log("user is ", user.natalSign);
+                });
+            }
+            else if(240 <= houses.ascendant < 270){
+                user.overwrite({natalSign: 'Sagittarius'}).save(() => {
+                    console.log("user is ", user.natalSign);
+                });
+            }
+            else if(270 <= houses.ascendant < 300){
+                user.overwrite({natalSign: 'Capricorn'}).save(() => {
+                    console.log("user is ", user.natalSign);
+                });
+            }
+            else if(300 <= houses.ascendant < 330){
+                user.overwrite({natalSign: 'Aquarius'}).save(() => {
+                    console.log("user is ", user.natalSign);
+                });
+            }
+            else if(330 <= houses.ascendant < 360){
+                user.overwrite({natalSign: 'Pisces'}).save(() => {
+                    console.log("user is ", user.natalSign);
                 });
             }
             else{
