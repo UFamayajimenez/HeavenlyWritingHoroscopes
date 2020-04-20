e65ae7aee1c81c4a1bde076311bf4790d7b27715
Logan Welsh <lrdwelsh98@gmail.com>
2020-04-20 12:56:35 -0400

    moonSign is a test file

--- server/modules/moonSign.js
+++ server/modules/moonSign.js
@@ -4,26 +4,24 @@ const swisseph = require('swisseph'),
 
 const currMoonSign = () => {
     swisseph.swe_julday(2020, 4, 19, 19, swisseph.SE_GREG_CAL, (julday) => {
-        swisseph.swe_calc_ut(julday, swisseph.SE_MOON, swisseph.SEFLG_SPEED, (body) => {
+        swisseph.swe_calc_ut(julday, swisseph.SE_MOON, 0, (body) => {
             console.log(body);
-            swisseph.swe_houses(julday, body.latitude, body.longitude, 'W', (houses) => {
-                console.log(houses);
-            })
-        });
+        })
     });
 };
 
 const currMoon = () => {
-    const result = SunCalc.getMoonPosition(new Date(), 29.651980, -82.325020);
+    const result = SunCalc.getMoonPosition(new Date(), 0, 0);
     const pi = Math.PI;
-    const degrees = result.azimuth * (180/pi);
+    const degrees = result.altitude * (180/pi);
     console.log(degrees);
+    console.log(360 + degrees);
 
-    swisseph.swe_julday(2020, 4, 19, 20, swisseph.SE_GREG_CAL, (julday) => {
-        swisseph.swe_houses(julday, 29.651980, -82.325020, 'W', (houses) => {
+    swisseph.swe_julday(2020, 4, 20, 12, swisseph.SE_GREG_CAL, (julday) => {
+        swisseph.swe_houses(julday, 0, 0, 'W', (houses) => {
             console.log(houses);
         })
     })
 };
 
-currMoon();
+currMoonSign();
